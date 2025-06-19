052864d15a9a185b933883dbf23ba599b80d0cc6
GIULIA SOUZA DA FONSECA <giulia_fonseca@estudante.sesisenai.org.br>
2025-06-18 21:32:35 -0300

    eu fiz um mini banco de dados que jaja arrumo

---
+++ server.js
@@ -0,0 +1,9 @@
+import app from './src/App.js'
+
+const port = 3000
+
+// porta 3000
+
+app.listen(port, () => {
+    console.log('Servidor funcionando corretamente')
+})
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import conexao from '../infra/conexao.js'
+
+const app = express()
+
+app.use(express.json())
+
+//criar rota
+
+app.get('/usuario', (req, res) =>{
+    const sql = "SELECT * FROM usuario"
+    conexao.query(sql,(erro, resultado) => {
+        if(erro) {
+            console.log(erro)
+        }else{
+          res.status(200).json(resultado) 
+        }
+    })
+} )
+
+app.post('/usuario', (req, res) =>{
+    const usuario = req.body
+    const sql = "INSERT INTO usuario SET ?"
+    conexao.query(sql, usuario, (erro, resultado) =>{
+        if(erro) {
+            console.log(erro)
+        }else{
+            res.status(200).json (resultado)
+        }
+    })
+} )
+
